411f5c7d415fbc8d7aa607b0b352fc945e973db7
Luana Faxina Wenceslau <ra120171@uem.br>
2024-02-01 10:03:25 -0300

    Otimização do código
    Otimização do código usando o IF e as condicionais ou (||) e 'e' (&&)

--- JavaScript para Web/aluramidi/script.js
+++ JavaScript para Web/aluramidi/script.js
@@ -17,18 +17,31 @@ document.querySelector('.tecla_clap').onclick = tocaClap;
 
 function tocaSom(idAudioInstrumento){
     document.querySelector(idAudioInstrumento).play();
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento && elemento.localName === 'audio'){
+        elemento.play();
+    } else{
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
+
 const listaTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-while(contador < listaTeclas.length){
-
+for(let contador = 0; contador < listaTeclas.length; contador ++){
     const tecla = listaTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;  //template String
 
     tecla.onclick = function (){
         tocaSom(idAudio);
-}
-    contador ++;
+    }
+    tecla.onkeydown = function (event){
+        if(event.code === 'Space' || event.code === 'Enter'){
+             tecla.classList.add('ativa');
+        }     
+    }
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
+    }
 }
