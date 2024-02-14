f04480999e1c81d40035c8d6c816c9fbd7cd7a1f
Luana Faxina Wenceslau <ra120171@uem.br>
2024-02-14 17:16:09 -0300

    Classes
    Criando e entendendo o funcionamento de classes em Dart

--- Projetos em Dart do Zero/orientacao_objeto/bin/orientacao_objeto.dart
+++ Projetos em Dart do Zero/orientacao_objeto/bin/orientacao_objeto.dart
@@ -4,12 +4,58 @@ void main() {
   String cor = "Verde e Amarela";
   String sabor = "Doce e cítrica";
   int diasColheita = 40;
-  bool isMadura;
+  bool isMadura = funcEstaMadura(diasColheita);
 
-  if(diasColheita >= 30){
-    isMadura = true;
+  mostrarMadura("Uva", 35, cor: "Roxa");
+
+  Fruta fruta01 = Fruta(nome, peso, cor, sabor, diasColheita);
+  Fruta fruta02 = Fruta("Uva", 40, "Roxa", "Doce", 20);
+  
+  fruta02.estaMadura(60);
+}
+
+class Fruta {
+  String nome;
+  double peso;
+  String cor;
+  String sabor;
+  int diasColheita;
+  bool? isMadura;
+
+  Fruta(this.nome, this.peso, this.cor, this.sabor, this.diasColheita, {this.isMadura}); //Construtor
+  estaMadura (int diasParaMadurar){
+    isMadura = diasColheita >= diasParaMadurar;
+    print("A sua $nome foi colhida a $diasColheita dias, e precisa de $diasParaMadurar para poder comer. Ela está madura? $isMadura.");
+  }
+}
+
+funcDiasMadura(int dias){
+  int diasParaMadurar = 30;
+  int quantosDiasFaltam = diasParaMadurar - dias;
+  return quantosDiasFaltam;
+}
+
+/*PARAMETROS
+Posicionais Obrigatórios
+Nomeados Opcionais
+Parametros com "Padrão"
+Modificador "required"*/
+mostrarMadura(String nome, int dias,{String? cor = "sem cor definida"}){
+  if(dias >= 30){
+    print("A $nome está madura");
   }else{
-    isMadura = false;
+    print("A $nome não está madura");
   }
 
+  if(cor != null){
+    print("A $nome é $cor");
+  }
+}
+
+bool funcEstaMadura(int dias){
+    if(dias >= 30){
+      return true;
+    }else{
+      return false;
+    }
 }
