3e1f1fb0f9d716371a0f51e45f1d034129f67375
Luana Faxina Wenceslau <ra120171@uem.br>
2024-02-17 14:11:26 -0300

    Herança e Polimorfismo
    Conceitos de Herança e Polimorfismo em Dart

--- Projetos em Dart do Zero/orientacao_objeto/bin/orientacao_objeto.dart
+++ Projetos em Dart do Zero/orientacao_objeto/bin/orientacao_objeto.dart
@@ -1,32 +1,17 @@
 void main() {
-  String nome = "Laranja";
-  double peso = 100.2;
-  String cor = "Verde e Amarela";
-  String sabor = "Doce e cítrica";
-  int diasColheita = 40;
-  bool isMadura = funcEstaMadura(diasColheita);
 
   mostrarMadura("Uva", 35, cor: "Roxa");
 
-  Fruta fruta01 = Fruta(nome, peso, cor, sabor, diasColheita);
-  Fruta fruta02 = Fruta("Uva", 40, "Roxa", "Doce", 20);
+  Fruta fruta01 = Fruta("Uva", 40, "Roxa", "Doce", 20);
   
-  fruta02.estaMadura(60);
-}
+  fruta01.estaMadura(60);
 
-class Fruta {
-  String nome;
-  double peso;
-  String cor;
-  String sabor;
-  int diasColheita;
-  bool? isMadura;
+  Legumes mandioca = Legumes('Macaxeira', 1200, 'Marrom', true);
+  Fruta banana = Fruta('Banana', 75, 'Amarela', 'Doce', 12);
 
-  Fruta(this.nome, this.peso, this.cor, this.sabor, this.diasColheita, {this.isMadura}); //Construtor
-  estaMadura (int diasParaMadurar){
-    isMadura = diasColheita >= diasParaMadurar;
-    print("A sua $nome foi colhida a $diasColheita dias, e precisa de $diasParaMadurar para poder comer. Ela está madura? $isMadura.");
-  }
+  mandioca.printAlimento();
+  banana.fazerSuco();
+  banana.fazerMassa();
 }
 
 funcDiasMadura(int dias){
@@ -58,4 +43,94 @@ bool funcEstaMadura(int dias){
     }else{
       return false;
     }
+}
+
+//Conceito de Herança
+class Alimento{
+  String nome;
+  double peso;
+  String cor;
+  Alimento(this.nome, this.peso, this.cor);
+
+  void printAlimento(){
+    print('Este $nome pesa $peso gramas e é $cor');
+  }
+}
+
+class Fruta extends Alimento implements Bolo{
+  String sabor;
+  int diasColheita;
+  bool? isMadura;
+
+  Fruta(String nome, double peso, String cor, this.sabor, this.diasColheita, {this.isMadura})
+    :super(nome, peso, cor); //Construtor
+  estaMadura (int diasParaMadurar){
+    isMadura = diasColheita >= diasParaMadurar;
+    print("A sua $nome foi colhida a $diasColheita dias, e precisa de $diasParaMadurar para poder comer. Ela está madura? $isMadura.");
+  }
+  void fazerSuco(){
+      print('Você fez um suco de $nome');
+    }
+
+  @override
+  void separarIngredientes(){
+    print('Pegar a $nome');
+  }
+
+  @override
+    void fazerMassa(){
+      print('Misturar os ingredientes');
+    }
+
+  @override
+    void assar(){
+      print('Colocar no forno');
+    }
+}
+
+class Legumes extends Alimento implements Bolo{
+  bool isPrecisaCozinhar;
+  Legumes(String nome, double peso, String cor, this.isPrecisaCozinhar)
+    :super(nome, peso, cor);
+
+  void cozinhar(){
+    if(isPrecisaCozinhar){
+      print('Pronto, o $nome está cozinhando!');
+    }else{
+      print('Não precisa cozinhar');
+    }
+  }
+  @override
+  void assar(){}
+
+  @override
+    void fazerMassa(){}
+
+  @override
+    void separarIngredientes(){}
+}
+
+class Citricas extends Fruta{
+  double nivelAzedo;
+  Citricas(String nome, double peso, String cor, String sabor, int diasColheita, this.nivelAzedo)
+    :super(nome, peso, cor, sabor, diasColheita);
+}
+
+class Nozes extends Fruta{
+  double porcentagemOleo;
+  Nozes(String nome, double peso, String cor, String sabor, int diasColheita, this.porcentagemOleo)
+    :super(nome, peso, cor, sabor, diasColheita);
+
+  @override
+    void fazerMassa(){
+      print('Tirar a casca');
+      super.fazerMassa();
+    }
+}
+
+//conceito classe abstrata
+abstract class Bolo{
+  void separarIngredientes();
+  void fazerMassa();
+  void assar();
 }
