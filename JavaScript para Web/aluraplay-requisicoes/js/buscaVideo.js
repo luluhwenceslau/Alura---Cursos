2a45194b00d49780cc65ca6cbafedb3ee5149c17
Luana Faxina Wenceslau <ra120171@uem.br>
2024-03-11 13:24:16 -0300

    criando requisições com JS

---
+++ JavaScript para Web/aluraplay-requisicoes/js/buscaVideo.js
@@ -0,0 +1,21 @@
+import { conectaApi } from "./conectaApi.js";
+import constroiCard from "./mostrarVideos.js";
+
+async function buscaVideo(evento){
+    evento.preventDefault();
+
+    const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
+    const busca = await conectaApi.buscaVideo(dadosDePesquisa);
+
+    const lista = document.querySelector("[data-lista]")
+
+    while(lista.firstChild){
+        lista.removeChild(lista.firstChild);
+    }
+
+    busca.forEach(elemento => lista.appendChild
+        (constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
+}
+const botaoPesquisa = document.querySelector("[data-botao-pesquisa]");
+
+botaoPesquisa.addEventListener("click", evento => buscaVideo(evento))
