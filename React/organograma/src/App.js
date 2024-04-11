e83ff9c9ef17d79a1e0d461b019131236c245754
Luana Faxina Wenceslau <ra120171@uem.br>
2024-04-11 11:00:48 -0300

    Criação do Formulario com React

--- React/organograma/src/App.js
+++ React/organograma/src/App.js
@@ -1,9 +1,12 @@
 import Banner from './componentes/Banner/Banner';
+import CampoTexto from './componentes/CamposTexto/CampoTexto';
+import Formulario from './componentes/Formulario';
 
 function App() {
   return (
     <div className="App">
       <Banner/> 
+      <Formulario />
     </div>
   )
 }
