dbd5032daf2f801d6b0e9917d4c847d37e63416f
Luana Faxina Wenceslau <113054076+luluhwenceslau@users.noreply.github.com>
2024-07-09 11:42:48 -0300

    Add files via upload

---
+++ Flutter/alubank/lib/screens/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../components/sections/header.dart';
+
+class Home extends StatelessWidget {
+const Home({ super.key });
+
+  @override
+  Widget build(BuildContext context){
+    return const Scaffold(
+      body: Column(
+        children: <Widget>[
+          Header(),
+        ],
+      ),
+    );
+  }
+}
